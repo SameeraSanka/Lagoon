62f0a94bb4e34b07e325af62bc4c34dc8f6ebb45
Sameera Madusanka <109691387+SameeraSanka@users.noreply.github.com>
2024-10-09 15:15:26 +0530

    Add project files.

---
+++ Lagoon.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lagoon.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
